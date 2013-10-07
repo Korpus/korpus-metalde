89951da8c3805c86d631288b61baaa6139beb022
Florian Weber <florian.weber.dd@me.com>
2013-10-07 12:52:19 +0200

    deleted acme bundle + added main page

---
+++ src/Korpus/MainPageBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Korpus\MainPageBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('KorpusMainPageBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Korpus/MainPageBundle/KorpusMainPageBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Korpus\MainPageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KorpusMainPageBundle extends Bundle
+{
+}
