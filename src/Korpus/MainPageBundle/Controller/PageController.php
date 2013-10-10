7912d26d87c7066563b54c1cc3e6733dd17c1b90
Florian Weber <florian.weber.dd@me.com>
2013-10-10 12:57:16 +0200

    news text markdown rendering

--- src/Korpus/MainPageBundle/Controller/PageController.php
+++ src/Korpus/MainPageBundle/Controller/PageController.php
@@ -3,7 +3,6 @@
 namespace Korpus\MainPageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Korpus\DataBundle\Entity\NewsPost;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PageController extends Controller
