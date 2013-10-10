bce056edb25d94c5d25a4fb844dc85d4ef63d306
Florian Weber <florian.weber.dd@me.com>
2013-10-10 13:34:01 +0200

    news toolbox

--- web/assets/pages/korpusconsole/js/app.js
+++ web/assets/pages/korpusconsole/js/app.js
@@ -1,8 +1,46 @@
 
+var addlink = function(text, url, title) {
+    var tbox = $('#form_text');
+    var currentContent = tbox.value();
+    var link = '';
+
+    if (title === '')
+        link = '[' + text + '](' + url + ')';
+    else
+        link = '[' + text + '](' + url + ' "' + title + '")';
+
+    tbox.value(currentContent + link);
+}
+
 $(document).ready(function() {
 
     $('.nav-link').removeClass('active');
     $('.nl-' + $('#current-page').html()).addClass('active');
     $('#nls-' + $('#current-subpage').html()).addClass('active');
+
+    //news toolbox
+    $('#btn-add-link').popover({
+        html: true,
+        placement: 'bottom',
+        title: function() {
+            return 'Link hinzufügen';
+        },
+        content: function() {
+            return $('#add-link-content').html();
+        }
+    });
+
+    $('form-add-link').livequery('submit', function(e) {
+        e.preventDefault();
+        var text = $(this).children('#input-add-link-text').value();
+        var url = $(this).children('#input-add-link-url').value();
+        var title = $(this).children('#input-add-link-title').value();
+
+        if (text !== '' && url !== '') {
+            addlink(text, url, title);
+        } else {
+            alertify.error('Text und URL dürfen nicht leer sein!');
+        }
+    });
 
 });
