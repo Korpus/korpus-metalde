7888697afffb4b19e210ae045ccda19571c8412e
Florian Weber <florian.weber.dd@me.com>
2013-10-29 11:56:51 +0100

    no log width _wdt

--- src/Korpus/LoggingBundle/Service/AccessLogger.php
+++ src/Korpus/LoggingBundle/Service/AccessLogger.php
@@ -5,24 +5,31 @@ namespace Korpus\LoggingBundle\Service;
 use Korpus\LoggingBundle\Entity\AccessLog;
 use Doctrine\ORM\EntityManager;
 
-class AccessLogger {
+class AccessLogger
+{
 
     private $em;
 
-    public function __construct(EntityManager $entityManager) {
+    public function __construct(EntityManager $entityManager)
+    {
         $this->em = $entityManager;
     }
 
-    public function log($source_ip, $target_resource, $referer) {
-        $accessLog = new AccessLog();
+    public function log($source_ip, $target_resource, $referer)
+    {
+        if (stripos($target_resource, '_wdt') > -1) {
+            
+        } else {
+            $accessLog = new AccessLog();
 
-        $accessLog->setDate(new \DateTime('now'));
-        $accessLog->setReferer($referer);
-        $accessLog->setSourceIp(md5($source_ip));
-        $accessLog->setTargetResource($target_resource);
+            $accessLog->setDate(new \DateTime('now'));
+            $accessLog->setReferer($referer);
+            $accessLog->setSourceIp(md5($source_ip));
+            $accessLog->setTargetResource($target_resource);
 
-        $this->em->persist($accessLog);
-        $this->em->flush();
+            $this->em->persist($accessLog);
+            $this->em->flush();
+        }
     }
 
 }
