66d20916a491d0716baea47fd63846b63a50f31c
Florian Weber <florian.weber.dd@icloud.com>
2013-11-07 15:45:13 +0100

    user add form

--- src/Korpus/ConsoleBundle/Controller/PageController.php
+++ src/Korpus/ConsoleBundle/Controller/PageController.php
@@ -3,6 +3,8 @@
 namespace Korpus\ConsoleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Korpus\UserBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 
 class PageController extends Controller
 {
@@ -14,6 +16,7 @@ class PageController extends Controller
 
     public function dashboardAction()
     {
+        //news
         $sourceLogs = $this->getDoctrine()->getRepository('KorpusDataBundle:SourceLog')->findAllPerNewsPost();
 
         $logs = array();
@@ -25,24 +28,42 @@ class PageController extends Controller
                 'source' => $log[0]->getSource()
             );
         }
-        
+
         //serverinfo
         $currentUsage = disk_total_space('/') - disk_free_space('/');
         $maxSize = disk_total_space('/');
         $percentage = number_format(($currentUsage / $maxSize) * 100, 2, '.', '');
         $freeSpace = disk_free_space('/');
-        
+
         $currentUsageMB = $currentUsage / 1000000;
         $maxSizeMB = $maxSize / 1000000;
         $freeSpaceMB = $freeSpace / 1000000;
+
+        //users
+        $users = $this->getDoctrine()->getRepository('KorpusUserBundle:User')->findAll();
 
         return $this->render('KorpusConsoleBundle:Page:dashboard.html.twig', array(
                     'logs' => $logs,
                     'percentage' => $percentage,
                     'current_usage' => number_format($currentUsageMB, 2, ',', '.'),
                     'max_size' => number_format($maxSizeMB, 2, ',', '.'),
-                    'free_space' => number_format($freeSpaceMB, 2, ',', '.')
+                    'free_space' => number_format($freeSpaceMB, 2, ',', '.'),
+                    'users' => $users
         ));
+    }
+
+    public function addUserAction(Request $request)
+    {
+        if ($request->get('username') !== "" && $request->get('username') !== null && $request->get('email') !== "" && $request->get('email') !== null && $request->get('password') !== "" && $request->get('password') !== null) {
+            $user = new User();
+            $user->setUsername($request->get('username'))->setEmail($request->get('email'))->setPlainPassword($request->get('password'));
+
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($user);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('korpus_console_dashboard'));
     }
 
 }
