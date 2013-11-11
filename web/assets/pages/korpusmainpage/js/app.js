f83ab8b47aad2bbbc020d0e2797911078c29da15
Florian Weber <florian.weber.dd@icloud.com>
2013-11-11 21:44:30 +0100

    bookblock

--- web/assets/pages/korpusmainpage/js/app.js
+++ web/assets/pages/korpusmainpage/js/app.js
@@ -20,24 +20,26 @@ $(document).ready(function() {
         albumid: '5945065822900542385',
         imgmax: $('.content-area').width()
     });
-    
+
     //hd 13
     $("#slide2").googleslides({
         userid: '116997391993592039168',
         albumid: '5945071918379232177',
         imgmax: $('.content-area').width()
     });
-    
+
     //sni 13
     $("#slide3").googleslides({
         userid: '116997391993592039168',
         albumid: '5945073303979243089',
         imgmax: $('.content-area').width()
     });
-    
+
     $.facebox.settings.closeImage = $('#fbox-closelabel').html();
     $.facebox.settings.loadingImage = $('#fbox-loading').html();
 
     $('a[rel*=soundcloud-facebox]').facebox();
+
+    $('#bb-ebenbild').bookblock();
 
 });
