7f326ad023ca418c36e3345c2003943f380bafd5
Florian Weber <florian.weber.dd@icloud.com>
2013-12-10 21:00:00 +0100

    backup command folder backup

--- src/Korpus/BackupBundle/Command/BackupDatabaseCommand.php
+++ src/Korpus/BackupBundle/Command/BackupDatabaseCommand.php
@@ -2,13 +2,15 @@
 
 namespace Korpus\BackupBundle\Command;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Korpus\BackupBundle\Component\BackupMaker;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
-class BackupDatabaseCommand extends Command
+class BackupDatabaseCommand extends ContainerAwareCommand
 {
 
     protected function configure()
@@ -30,9 +32,9 @@ class BackupDatabaseCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $database = $input->getArgument('database');
-        $allDB = false;
+        $allDB = true;
         if ($database)
-            $allDB = true;
+            $allDB = false;
 
         $target = $input->getArgument('target');
         if (!$target) {
@@ -40,11 +42,39 @@ class BackupDatabaseCommand extends Command
             return;
         }
 
+        if (substr($target, -1) != DIRECTORY_SEPARATOR)
+            $target .= DIRECTORY_SEPARATOR;
+
         $targettype = $input->getArgument('targettype');
         if (!in_array(strtolower($targettype), array('mail', 'folder'))) {
             $output->writeln('Target Type not matched!');
             return;
         } else {
+            $host = $this->getContainer()->getParameter('database_host');
+            $user = $this->getContainer()->getParameter('database_user');
+            $password = $this->getContainer()->getParameter('database_password') != 'null' ? $this->getContainer()->getParameter('database_password') : '';
+
+            $fs = new Filesystem();
+            $dumps = array();
+
+            if (!$allDB) {
+                $dumps[] = BackupMaker::backupTables($host, $user, $password, $database);
+                $output->writeln($allDB);
+            } else {
+                $output->writeln($allDB);
+            }
+            
+            $output->writeln($target);
+
+            foreach ($dumps as $dump) {
+                if ($targettype == 'folder') {
+                    $fs->dumpFile($target . 'db-backup-' . $database . '-' . time() . '.sql', $dump);
+                    $output->writeln($target . 'db-backup-' . $database . '-' . time() . '.sql');
+                } else if ($targettype == 'mail') {
+                    
+                }
+            }
+
             $output->writeln('Saved!');
         }
     }
